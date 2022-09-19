a28270dfccdc196e3459ef15c995e42725cb17b5
Nickoscos <nicolas.pelleschi@gmail.com>
2022-09-19 23:05:21 +0200

    VALIDATOR express en cours

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ require('./config/.env');
 const tauxFct = require('./assets/js/tauxDevise.js');
 const imcFct = require('./assets/js/calculIMC.js');
 
-const { check, validationResult }
-    = require('express-validator');
-const bodyparser = require('body-parser')
+
+const bodyParser = require('body-parser');
 
 //Création du serveur local
-const express = require('express')
-const app = express()
-const port = 3331
+const express = require('express');
+const app = express();
+const port = 3331;
 
 let http = require('http');
 let fs = require('fs');
@@ -23,7 +22,10 @@ const { Server } = require("socket.io");
 //Chemin du fichier contenant les taux de devise
 let pathFile = 'src/assets/json/taux.json';
 
-
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 //Routage vers page accueil
 app.get('/', (request, response) => {
@@ -76,101 +78,33 @@ app.post('/convDevise', (request, response) => {
     });
 })
 
-//Routage POST page inscription
-app.post('/inscription', 
-    body('email').custom(value => {
-        return User.findUserByEmail(value).then(user => {
-        if (user) {
-            return Promise.reject('E-mail already in use');
-        }
-        });
-    }),
-    (request, response) => {
-        fs.readFile(path.join(_dirnamePages + 'pages/inscription.html'), function (error, data) {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(data);
-            response.end();
+//Routage vers page inscription
+app.get('/inscription', (request, response) => {
+    fs.readFile(path.join(_dirnamePages + 'pages/inscription.html'), function (error, data) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
     });
 })
+
+//Routage POST page inscription
+const { check, validationResult } = require('express-validator');
+app.post('/inscription', 
+    check('email').isEmail(),
+    (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            console.log(({ errors: errors.array() }));
+            // console.log(response.status(400).json({ errors: errors.array() }));
+        }
+    });
 
 //Routage vers les fichiers CSS 
 app.use(express.static(_dirnamePages + '/assets'));
 
 //Création du serveur HTTP
 const server = http.createServer(app);
-// let server = http.createServer((request, response) => {
-   
-//     let url = request.url;
-//     let extname = path.extname(url);
-//     if(url === '/'){
-//         fs.readFile(_dirnamePages + 'pages/accueil.html', null, function (error, data) {
-//             response.writeHead(200, { 'Content-Type': 'text/html' });
-//             response.write(data);
-//             response.end();
-//         });
-//     }
-//     else if (url === '/IMC.js'){
-//         response.writeHead(200);
-//         fs.readFile(path.join(_dirnamePages + 'IMC.js'), function (error, data) {
-//             response.writeHead(200, { 'Content-Type': 'text/javascript' });
-//             response.write(data);
-//             response.end();
-//         });
-//     }
-//     else if (url === '/scriptDevise.js'){
-//         response.writeHead(200);
-//         fs.readFile(path.join(_dirnamePages + 'scriptDevise.js'), function (error, data) {
-//             response.writeHead(200, { 'Content-Type': 'text/javascript' });
-//             response.write(data);
-//             response.end();
-//         });
-//     }
-//     else if(url === '/calculImc'){
-//         response.writeHead(200);
-//         fs.readFile(path.join(_dirnamePages + 'pages/calculImc.html'), function (error, data) {
-//             response.writeHead(200, { 'Content-Type': 'text/html' });
-//             response.write(data);
-//             response.end();
-//         });
-//     }
-//     else if((url === '/convDevise')){
-//         response.writeHead(200);
-//         fs.readFile(path.join(_dirnamePages + 'pages/convDevise.html'), function (error, data) {
-//             response.writeHead(200, { 'Content-Type': 'text/html' });
-//             response.write(data);
-//             response.end();
-//         });
-//         //Détection envoi formulaire
-//         if(request.method == 'POST') {
-//             request.on('data',  (data) => { 
-//                 tauxFct.change(pathFile, data);
-//             });
-//         }
-//     }
-//     else if((url === '/inscription')){
-//         response.writeHead(200);
-//         fs.readFile(path.join(_dirnamePages + 'pages/inscription.html'), function (error, data) {
-//             response.writeHead(200, { 'Content-Type': 'text/html' });
-//             response.write(data);
-//             response.end();
-//         });
-//         //Détection envoi formulaire
-//         if(request.method == 'POST') {
-//             request.on('data',  (data) => { 
-//                 console.log("Soummission du formulaire d'inscription")
-//             });
-//         }
-//     }
-//     else if(extname === '.png'){
-//         response.writeHead(200, { 'Content-Type': 'image/png' });
-//         fs.createReadStream(`${_dirnamePages}${url}`).pipe(response);
-//     }
-//     else if(extname === '.css'){
-//         response.writeHead(200, { 'Content-Type': 'text/css' });
-//         fs.createReadStream(`${_dirnamePages}${url}`).pipe(response);
-//     }
 
-// });
 
 //Socket permet le lien entre le script js du browser client et le server 
 const io = new Server(server);
